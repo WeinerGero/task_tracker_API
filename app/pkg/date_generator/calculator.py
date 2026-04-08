6af2acf324bccfefc21d5e61267f6590d4753550
WinerGero <korykin.27@mail.ru>
2026-04-09 00:28:17 +0200

    добавлена логика с растановкой тасков до конкретной даты по каждому из способов распределения тасков

--- app/pkg/date_generator/calculator.py
+++ app/pkg/date_generator/calculator.py
@@ -2,6 +2,22 @@ from datetime import date, timedelta
 from enums import DateType as RecurrenceType
 
 
+def _check_date_validity(start_date: date, end_date: date) -> None:
+    """
+    Проверяет валидность начальной и конечной дат.
+    
+    Args:
+        start_date (date): Начальная дата.
+        end_date (date): Конечная дата.
+
+    Raises:
+        ValueError: Если начальная дата больше конечной даты.
+    """
+    if start_date > end_date:
+        raise ValueError("Начальная дата должна быть меньше или равна конечной дате.")
+    if (end_date - start_date).days > 365 * 2:
+        raise ValueError("Разница между начальной и конечной датой не должна превышать 5 лет.")
+    
 def _get_daily_dates(start_date: date, config: dict) -> list[date]:
     """
     Генерирует следующие даты на основе ежедневного правила.
@@ -10,11 +26,19 @@ def _get_daily_dates(start_date: date, config: dict) -> list[date]:
         start_date (date): Дата, с которой начинается генерация.
         config (dict): Конфигурация, содержащая необходимые параметры для генерации дат (например, количество дат, интервалы и т.д.).
 
+    Raises:
+        ValueError: Если дата окончания меньше начальной даты.
+    
     Returns:
         list[date]: Список сгенерированных дат.
     """
     date_count = config.get("count", 1)
     interval = config.get("interval", 1)
+
+    if config.get("end_date"):
+        days_left = (config["end_date"] - start_date).days + 1
+        date_count = (days_left + interval - 1) // interval
+    
     return [start_date + timedelta(days=i * interval) for i in range(date_count)]
 
 def _get_monthly_dates(start_date: date, config: dict) -> list[date]:
@@ -46,8 +70,16 @@ def _get_monthly_dates(start_date: date, config: dict) -> list[date]:
     if year := start_date.year:
         month_days[2] = 29 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) else 28
     
+    if config.get("end_date"):
+        days_left = (config["end_date"] - start_date).days + 1
+        date_count = (days_left + 29) // 30
+    
     return [
-        date(start_date.year + (start_date.month + i) // 12, (start_date.month + i) % 12 or 12, min(start_date.day, month_days[(start_date.month + i) % 12 or 12]))
+        date(
+            start_date.year + (start_date.month + i) // 12,
+            (start_date.month + i) % 12 or 12,
+            min(start_date.day, month_days[(start_date.month + i) % 12 or 12])
+            )
         for i in range(date_count)
     ]
 
@@ -85,6 +117,10 @@ def _get_even_dates(start_date: date, config: dict) -> list[date]:
     start_date = start_date if start_date.day % 2 == 0 else start_date + timedelta(days=1)
     date_count = config.get("count", 1)    
 
+    if config.get("end_date"):
+        days_left = (config["end_date"] - start_date).days + 1
+        date_count = (days_left) // 2
+
     return [
         start_date + timedelta(days=i * 2) 
         if (start_date + timedelta(days=i * 2)).day % 2 == 0 
@@ -105,6 +141,10 @@ def _get_odd_dates(start_date: date, config: dict) -> list[date]:
     """
     start_date = start_date if start_date.day % 2 != 0 else start_date + timedelta(days=1)
     date_count = config.get("count", 1)  
+    
+    if config.get("end_date"):
+        days_left = (config["end_date"] - start_date).days + 1
+        date_count = (days_left) // 2
     
     return [
         start_date + timedelta(days=i)
@@ -128,6 +168,9 @@ def get_next_dates(start_date: date, rule_type: RecurrenceType, config: dict) ->
     Returns:
         list[date]: Список сгенерированных дат.
     """
+    if config.get("end_date"):
+        _check_date_validity(start_date, config["end_date"])
+    
     if rule_type == RecurrenceType.DAILY:
         return _get_daily_dates(start_date, config)
     elif rule_type == RecurrenceType.MONTHLY:
@@ -144,9 +187,10 @@ def get_next_dates(start_date: date, rule_type: RecurrenceType, config: dict) ->
 
 if __name__ == "__main__":
     # Example usage
-    start_date = date(2024, 2, 1)
+    start_date = date(2024, 12, 30)
     config = {"count": 5,
-              "interval": 2,
+              "interval": 5,
+              "end_date": date(2025, 1, 15),
               "custom_dates": [date(2025, 1, 10), date(2025, 1, 20)],
               }  # Example configuration
     print(get_next_dates(start_date, RecurrenceType.DAILY, config))
