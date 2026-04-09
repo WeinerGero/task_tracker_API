f8aad7233832d4c75c3e4998e74edfc44e1ef756
WinerGero <korykin.27@mail.ru>
2026-04-09 22:50:25 +0200

    добавлена модель с шаблонами заданий для генерации дат

--- app/models/templates.py
+++ app/models/templates.py
@@ -1,3 +1,19 @@
 """
 Модуль, содержащий модели данных для шаблонов задач.
 """
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+
+class TaskTemplate(Base):
+    __tablename__ = "task_templates"
+
+    # Тип правила генерации дат, например:
+    # "daily", "monthly", "custom_dates", "even", "odd"
+    rule_type: Mapped[str] = mapped_column(String(20), nullable=False)
+
+    # Конфигурация для генерации дат, хранящаяся в формате JSON
+    rule_config: Mapped[dict] = mapped_column(JSONB, nullable=False)
