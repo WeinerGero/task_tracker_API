8002eeb788daee3a17da1eccb295e4e8ed05efa9
WinerGero <korykin.27@mail.ru>
2026-04-09 02:20:50 +0200

    с помощью dateutil.rrule обработал даты в зависимости от типа конфигурации моделей

--- app/pkg/date_generator/calculator.py
+++ app/pkg/date_generator/calculator.py
@@ -1,16 +1,18 @@
-from datetime import date, timedelta
+from datetime import date, datetime, time
+from logging import config
 from dateutil.rrule import rrule, DAILY, MONTHLY
 from typing import Literal
 
 from pydantic import BaseModel, Field, RootModel, model_validator
 from typing import Union, Annotated
 
+from config import EVEN_DAYS, MAX_COUNT, ODD_DAYS
 from enums import DateType as RecurrenceType
 
 
 class DateConfig(BaseModel):
     type: RecurrenceType
-    count: int | None = Field(gt=0, default=100)
+    count: int | None = Field(le=MAX_COUNT)
     start_date: date | None = Field(default_factory=date.today)
     end_date: date | None = None
     
@@ -56,5 +58,69 @@ RecurrenceConfig = Annotated[
     Union[DailyConfig, MonthlyConfig, CustomDatesConfig, EvenConfig, OddConfig], 
     Field(discriminator="type")
 ]
+
+
+def _get_rrule_dates(config: DateConfig) -> list[date]:    
+    # Генерируем даты в зависимости от типа конфигурации
+    match config:
+        # Генерация ежедневных дат
+        case DailyConfig():
+            rule = rrule(
+                    freq=DAILY,
+                    dtstart=datetime.combine(config.start_date, time.min),
+                    interval=config.interval,
+                    # Если указано конечное число, используем его, иначе - None для бесконечной генерации
+                    until=datetime.combine(config.end_date, time.max) 
+                    if config.end_date else None,
+                    count=config.count or MAX_COUNT
+                )
+            
+        # Генерация ежемесячных дат
+        case MonthlyConfig():
+            rule = rrule(
+                    freq=MONTHLY,
+                    dtstart=datetime.combine(config.start_date, time.min),
+                    # Если указано число месяца, используем его, иначе -1 для последнего дня месяца
+                    bymonthday=config.bymonthday 
+                    if config.bymonthday not in [29, 30, 31] else [-1],
+                    # Если указано конечное число, используем его, иначе - None для бесконечной генерации
+                    until=datetime.combine(config.end_date, time.max)
+                    if config.end_date else None,
+                    count=config.count or MAX_COUNT
+                )
+            
+        # Генерация пользовательских дат
+        case CustomDatesConfig():
+            return config.dates 
+              
+        # Генерация четных дат
+        case EvenConfig():
+            rule = rrule(
+                    freq=DAILY,
+                    dtstart=datetime.combine(config.start_date, time.min),
+                    # Если указано конечное число, используем его, иначе - None для бесконечной генерации
+                    until=datetime.combine(config.end_date, time.max)
+                    if config.end_date else None,
+                    bymonthday=EVEN_DAYS,
+                    count=config.count or MAX_COUNT
+                )
+            
+        # Генерация нечетных дат
+        case OddConfig():
+            rule = rrule(
+                    freq=DAILY,
+                    dtstart=datetime.combine(config.start_date, time.min),
+                    # Если указано конечное число, используем его, иначе - None для бесконечной генерации
+                    until=datetime.combine(config.end_date, time.max)
+                    if config.end_date else None,
+                    bymonthday=ODD_DAYS,
+                    count=config.count or MAX_COUNT
+                )
+            
+        # Если тип конфигурации неизвестен, выбрасываем исключение
+        case _:
+            raise ValueError("Unknown config type")
+            
+    return [dt.date() for dt in rule]
 
 
