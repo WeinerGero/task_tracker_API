f376fc57e4c9c9099e00810199d7b16b0afa81aa
WinerGero <korykin.27@mail.ru>
2026-04-11 00:38:00 +0200

    создал асинхронный метод создания по модели, реализовал с ммоделью шаблона

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,10 +1,18 @@
 """
 
 """
+from typing import TypeVar, Generic
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
+T = TypeVar("T")
 
-class BaseRepository:
+
+class BaseRepository(Generic[T]):
     def __init__(self, session: AsyncSession):
         self.session = session
 
+    async def create(self, model: T) -> T:
+        self.session.add(model)
+        await self.session.flush()
+        return model
--- app/repositories/template_repo.py
+++ app/repositories/template_repo.py
@@ -1,4 +1,10 @@
 """
 
 """
+from app.models.templates import TaskTemplate
+from app.repositories.base import BaseRepository
 
+
+class TemplateRepository(BaseRepository):
+    def __init__(self, session):
+        super().__init__(session)
